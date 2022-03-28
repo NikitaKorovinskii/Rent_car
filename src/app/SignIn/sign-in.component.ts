fa3b60f00aa2e9c0e959c149438b3c69f2af113f
Nikitakorovinskii <78597438+Nikitakorovinskii@users.noreply.github.com>
2022-03-28 11:20:03 +0300

    initial commit

---
+++ src/app/SignIn/sign-in.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-SignIn',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
+})
+export class SignInComponent implements OnInit {
+
+  constructor() { }
+  SignUp(){
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/SignUp/sign-up.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-SignUp',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+  constructor() {
+  }
+
+  SignIn() {
+  }
+
+
+  ngOnInit(): void {
+  }
+
+}
